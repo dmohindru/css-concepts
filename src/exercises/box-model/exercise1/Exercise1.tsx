92f0d58870675da8283d3905b33058dd1c0b842d
Dhruv Mohindru <dmohindru@gmail.com>
2025-08-27 19:04:24 +1000

    Completed exercise 1

--- src/exercises/box-model/exercise1/Exercise1.tsx
+++ src/exercises/box-model/exercise1/Exercise1.tsx
@@ -8,7 +8,23 @@ const Exercise1: React.FC = () => {
   return (
     <div className="container">
       <ExerciseTitle title="Box Model" content={exerciseTitle} />
-      <div className={styles.parent}></div>
+      <div className={styles.parent}>
+        <div className={`${styles.boxStyle} ${styles.first}`}>
+          <p>
+            I have <br></br>Margin: 10px <br></br>Padding: 50px
+          </p>
+        </div>
+        <div className={`${styles.boxStyle} ${styles.second}`}>
+          <p>
+            I have <br></br>Margin Left: 50px <br></br>Padding: 10px
+          </p>
+        </div>
+        <div className={`${styles.boxStyle} ${styles.third}`}>
+          <p>
+            I have <br></br>Margin Left: -10px <br></br>Padding: 5px
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
