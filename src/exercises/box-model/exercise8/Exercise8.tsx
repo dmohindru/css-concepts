39a18549ef3d7310f0e7bdcb6ac4ce75a91aa0cb
Dhruv Mohindru <dmohindru@gmail.com>
2025-08-30 11:44:12 +1000

    Completed exercises 6, 7, 8

--- src/exercises/box-model/exercise8/Exercise8.tsx
+++ src/exercises/box-model/exercise8/Exercise8.tsx
@@ -3,25 +3,16 @@ import ExerciseTitle from "../../ExerciseTitle";
 import styles from "./exercise8.module.css";
 
 const Exercise8: React.FC = () => {
-  throw new Error("Not implemented");
-  const title = "Attribute Selectors";
+  const title = "Padding";
   const content =
-    "Style links based on their href attribute. Make links with href that starts with 'http' open in a new tab with a specific icon. Use [href^='http'] to apply styles.";
+    "Nest a smaller box within a larger box and apply padding to the parent container. Observe how padding affects the positioning of the inner element.";
   return (
     <div className="container">
       <ExerciseTitle title={title} content={content} />
-      <div className={styles.links}>
-        <a href="https://www.google.com" target="_blank">
-          Google
-        </a>
-        <br />
-        <a href="https://www.facebook.com">Facebook</a>
-        <br />
-        <a href="https://www.twitter.com">Twitter</a>
-        <br />
-        <a href="https://www.github.com">Github</a>
-        <br />
-        <a href="/home">Home</a>
+      <div className={styles.parent}>
+        <div className={styles.child}>
+          <div className={styles.grandChild}></div>
+        </div>
       </div>
     </div>
   );
