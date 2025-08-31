2cabe65ae503e3cf7a1fe4c4ee6fb1fae43a2c00
Dhruv Mohindru <dmohindru@gmail.com>
2025-09-01 09:24:28 +1000

    Completed exercises 9, 10, 11

--- src/exercises/box-model/exercise9/Exercise9.tsx
+++ src/exercises/box-model/exercise9/Exercise9.tsx
@@ -3,18 +3,16 @@ import ExerciseTitle from "../../ExerciseTitle";
 import styles from "./exercise9.module.css";
 
 const Exercise9: React.FC = () => {
-  throw new Error("Not implemented");
   return (
     <div className="container">
       <ExerciseTitle
-        title="Attribute Selectors"
-        content="Create a set of buttons with data-action attributes (like data-action='save', data-action='delete'). Use attribute selectors to style each button differently based on its data-action value."
+        title="Border"
+        content="Create a div and apply a solid border with a specified width, color, and style (solid, dashed, dotted, double, etc.). Experiment with different border-width and border-style values to observe the visual differences."
       />
-      <p>Solution</p>
-      <div className={styles.dataAction}>
-        <button data-action="save">Save</button>
-        <br />
-        <button data-action="delete">Delete</button>
+      <div className={styles.parent}>
+        <div className={`${styles.boxSize} ${styles.first}`}></div>
+        <div className={`${styles.boxSize} ${styles.second}`}></div>
+        <div className={`${styles.boxSize} ${styles.third}`}></div>
       </div>
     </div>
   );
