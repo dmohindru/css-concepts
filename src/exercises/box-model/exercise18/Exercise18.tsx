b6a323c34cbdd1236b967abf7d0039f28677e8a4
Dhruv Mohindru <dmohindru@gmail.com>
2025-09-10 09:15:53 +1000

    Completed exercise 16, 17, 18

--- src/exercises/box-model/exercise18/Exercise18.tsx
+++ src/exercises/box-model/exercise18/Exercise18.tsx
@@ -3,25 +3,14 @@ import ExerciseTitle from "../../ExerciseTitle";
 import styles from "./exercise18.module.css";
 
 const Exercise18: React.FC = () => {
-  throw new Error("Not implemented");
   return (
     <div className="container">
       <ExerciseTitle
-        title="Challenge Exercise: Combined Selectors"
-        content="Create an unordered list where every even list item (li:nth-child(even)) has a different text color. Additionally, apply a hover effect that changes the background color differently when the user hovers over odd and even list item."
+        title="Positioning with Margins and Padding"
+        content="Create a button with a shadow effect using margin and padding. Add a bottom margin and padding to create an 'elevated' look. Apply padding inside the button to increase the text spacing."
       />
-      <p>Solution</p>
-      <p>Unordered List</p>
-      <div className={styles.listStyle}>
-        <ul>
-          <li>First item</li>
-          <li>Second item</li>
-          <li>Third item</li>
-          <li>Fourth item</li>
-          <li>Fifth item</li>
-          <li>Sixth item</li>
-          <li>Seventh item</li>
-        </ul>
+      <div className={styles.parent}>
+        <button className={styles.myButton}>Click Me</button>
       </div>
     </div>
   );
