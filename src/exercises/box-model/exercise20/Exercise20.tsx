19d822df7c01e643637c1a2661ec15cb09bc5ce9
Dhruv Mohindru <dmohindru@gmail.com>
2025-09-12 09:20:40 +1000

    Building exercise 20

--- src/exercises/box-model/exercise20/Exercise20.tsx
+++ src/exercises/box-model/exercise20/Exercise20.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ExerciseTitle from "../../ExerciseTitle";
 import styles from "./exercise20.module.css";
+import profileImage from "./dm.jpeg";
 
 const Exercise20: React.FC = () => {
   return (
@@ -9,6 +10,21 @@ const Exercise20: React.FC = () => {
         title="Mini Project"
         content="Build a 'card' component with a title, image, and text. Use the box model to give it a consistent padding, add a border to separate each card visually, and use margin to add space between multiple cards."
       />
+      {/* <img src={profileImage}></img>
+      <img src={bgImage}></img> */}
+      <div className={styles.container}>
+        <div className={styles.cardBox}>
+          <div className={styles.cardImage}>
+            <div className={styles.title}>Dhruv Mohindru</div>
+            <div className={styles.position}>Senior Software Engineer</div>
+            <div className={styles.imageContainer}>
+              <img src={profileImage} />
+            </div>
+            <button className={styles.followButton}>Follow</button>
+          </div>
+          <div className={styles.cardText}></div>
+        </div>
+      </div>
     </div>
   );
 };
